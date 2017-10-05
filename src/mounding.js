10df2b29e9ebe33b4aeec2b9a2d5d16682d97a09
Ralf Junghanns <ralf.junghanns@gmail.com>
2017-10-05 16:07:03 +0200

    Update package.json

--- src/mounding.js
+++ src/mounding.js
@@ -28,6 +28,6 @@ export function calculateHi(x, y, w, L, W, hi, Sy, K, t) {
     return Math.sqrt(w / 2 / K * v * t * (s1 + s2 + s3 + s4) + hi * hi).toFixed(5) - hi.toFixed(5); // eq 1
 }
 
-export function calculateMax(w, L, W, hi, Sy, K, t) {
+export function calculateHMax(w, L, W, hi, Sy, K, t) {
     return calculateHi(0, 0, w, L, W, hi, Sy, K, t) + hi;
 }
--- test/library.spec.js
+++ test/library.spec.js
@@ -123,7 +123,31 @@ describe('Given the mounding hMax-function', () => {
         const expected = v[8].toFixed(decimals);
 
         it('calculating hMax', () => {
-            expect(mounding.calculateMax(w, L, W, hi, Sy, K, t).toFixed(decimals)).to.be.equal(expected);
+            expect(mounding.calculateHMax(w, L, W, hi, Sy, K, t).toFixed(decimals)).to.be.equal(expected);
+        });
+    });
+});
+
+describe('Given the mounding xMax-function', () => {
+    const values = [
+        [0.045, 40, 20, 35, 0.085, 1.8, 2, 2, 35.16],
+        [5.000, 40, 20, 35, 0.085, 1.8, 2, 2, 49.38],
+
+    ];
+
+    values.forEach( v => {
+        const w = v[0];
+        const L = v[1];
+        const W = v[2];
+        const hi = v[3];
+        const Sy = v[4];
+        const K = v[5];
+        const t = v[6];
+        const decimals = v[7];
+        const expected = v[8].toFixed(decimals);
+
+        it('calculating hMax', () => {
+            expect(mounding.calculateHMax(w, L, W, hi, Sy, K, t).toFixed(decimals)).to.be.equal(expected);
         });
     });
 });
